d3653f441f7bb95adaf527269395993964da5498
unknown <nanayawamefia98@gmail.com>
2021-08-18 05:18:17 -0800

    make it better

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ dotenv.config();
 
 const storage = multer.diskStorage({
     destination: (req, file, callback) =>{
-      callback(null, "../build/public/uploads/");
+      callback(null, "../build/uploads/");
     },
     filename: (req, file, callback) =>{
       callback(null, file.originalname);
