6606eb83dedaff81a410d20c1af00b1670556e10
unknown <nanayawamefia98@gmail.com>
2021-08-18 22:27:40 -0800

    make it better

--- server.js
+++ server.js
@@ -162,51 +162,48 @@ app.get("/", (req, res) => res.status(200).send("Hello its working"));
       
   });
 
-
-    //validate to login the user
-    app.post("/login", async (req, res) => {
-      try {
-        const { email, password } = req.body;
-    
-        // validate
-    
-        if (!email || !password)
-          return res
-            .status(400)
-            .json({ errorMessage: "Please enter all required fields." });
-    
-        const existingUser = await Signup.findOne({ email });
-        if (!existingUser)
-          return res.status(401).json({ errorMessage: "Wrong email or password." });
-    
-        const passwordCorrect =  await bcrypt.compare(password, existingUser.passwordhash)
-        if (!passwordCorrect)
-          return res.status(401).json({ errorMessage: "Wrong email or password." });
-    
-        // sign the token
-    
-        const token = jwt.sign(
-          {
-            user: existingUser._id,
-          },
-          process.env.JWT_SECRET
-        );
-    
-        // send the token in a HTTP-only cookie
-    
-       res
-          .cookie("token", token, {
-            httpOnly: true,
-            secure: true,
-            sameSite: "none",
-          })
-          .send();
-      } catch (err) {
-        console.error(err);
-        res.status(500).send();
-      }
-    });
-     
+  app.post("/login", async (req, res) => {
+    try {
+      const { email, password } = req.body;
+      
+      // validate
+      
+      if (!email || !password)
+        return res
+          .status(400)
+          .json({ errorMessage: "Please enter all required fields." });
+      
+      const existingUser = await Signup.findOne({ email });
+      if (!existingUser)
+        return res.status(401).json({ errorMessage: "Wrong email or password." });
+      
+      const passwordCorrect =  await bcrypt.compare(password, existingUser.passwordhash)
+      if (!passwordCorrect)
+        return res.status(401).json({ errorMessage: "Wrong email or password." });
+      
+      // sign the token
+      
+      const token = jwt.sign(
+        {
+          user: existingUser._id,
+        },
+        process.env.JWT_SECRET
+      );
+      
+      // send the token in a HTTP-only cookie
+      
+     res
+        .cookie("token", token, {
+          httpOnly: true,
+          secure: true,
+          sameSite: "none",
+        })
+        .send();
+    } catch (err) {
+      console.error(err);
+      res.status(500).send();
+    }
+  });
     // logout the user
    app.get("/logout", (req, res) => {
       res
