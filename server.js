6fa14dddfafa0483740ec9c8dd8a9d16a0219611
unknown <nanayawamefia98@gmail.com>
2021-08-18 23:57:11 -0800

    make it better

--- server.js
+++ server.js
@@ -1,84 +1,143 @@
-import express from 'express';
+import express from "express";
 import mongoose from 'mongoose';
-import Cors from "cors";
-import jwt from "jsonwebtoken";
+import Messages from "./dbMessages.js";
 import Signup from "./signup.js";
 import Pusher from "pusher";
+import cors from 'cors';
+import jwt from "jsonwebtoken";
 import bcrypt from "bcryptjs";
 import cookieParser from "cookie-parser";
 import dotenv from "dotenv";
 import multer from "multer";
-import Messages from "./dbMessages.js";
-import crypto from "crypto";
 
 dotenv.config();
 
 
 
+// app config
+
+const app = express();
+const port = process.env.PORT || 9000;
+
 const storage = multer.diskStorage({
-    destination: (req, file, callback) =>{
-      callback(null, "../tinder-clone/public/uploads/");
-    },
-    filename: (req, file, callback) =>{
-      callback(null, file.originalname);
-    }
-  })
-  
-  const upload = multer({storage: storage});
-  const pusher = new Pusher({
+  destination: (req, file, callback) =>{
+    callback(null, "../whatsapp-mern/public/uploads/");
+  },
+  filename: (req, file, callback) =>{
+    callback(null, file.originalname);
+  }
+})
+
+const upload = multer({storage: storage});
+
+const pusher = new Pusher({
     appId: "1225889",
     key: "d954810e751705611020",
     secret: "d1b7272f93ea266f206f",
     cluster: "eu",
     useTLS: true
   });
-// App config
-const app = express();
-const port = process.env.PORT || 8001;
 
-
-// Middlewares
+//  middlewares
 app.use(express.json());
-app.use(Cors({origin: ["http://localhost:3000", "https://tinder-clon.netlify.app"], credentials: true}));
+app.use(cors({
+    origin: ["http://localhost:3000", "https://tinder-clon.netlify.app/"],
+    credentials: true
+}));
 app.use(cookieParser());
 
 // DB config
 const connection_url = process.env.MONGODB_CONNECTION;
 mongoose.connect(connection_url, {
-    useNewUrlParser: true,
     useCreateIndex: true,
+    useNewUrlParser: true,
     useUnifiedTopology: true,
-})
+});
 
 const db = mongoose.connection;
- db.once("open", () => {
-  console.log('DB is connected');
-  const msgCollection = db.collection('messagecontents');
-  const changeStream = msgCollection.watch();
-     
-     changeStream.on('change', (change) => {
-      console.log(change);
-     if (change.operationType === "insert") {
-      const messageDetails = change.fullDocument;
-      pusher.trigger("messages", "inserted", {
-          message: messageDetails.message,
-          receiverId: messageDetails.receiverId,
-          senderId: messageDetails.senderId,
-      });
-     }else{
-       console.log("Error triggering pusher");
-     }
+
+db.once("open", () => {
+    console.log('DB connected');
+
+    const msgCollection = db.collection('messagecontents');
+    const changeStream = msgCollection.watch();
+
+
+
+    changeStream.on('change', (change) => {
+        console.log(change);
+
+        if (change.operationType === "insert") {
+            const messageDetails = change.fullDocument;
+            pusher.trigger("messages", "inserted", {
+                message: messageDetails.message,
+                receiverId: messageDetails.receiverId,
+                senderId: messageDetails.senderId,
+            });
+        }else{
+            console.log("Error triggering pusher");
+        }
     });
-  })
-
-//  API Endpoints
-app.get("/", (req, res) => res.status(200).send("Hello its working"));
+});
 
 
+// api routes
+app.get('/', (req, res) =>res.status(200).send('hello world'));
 
+app.get('/messages/:chatUserId', async (req, res) => {
+
+  try {
+    const chatmessages = await Messages.find({
+      $or :[
+         {senderId: req.params.chatUserId},
+         {receiverId: req.params.chatUserId},
+      ]
+    });
+    res.status(200).json(chatmessages);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+app.get('/messages/:sender/:receiver', async (req, res) => {
+  try {
+    const conversations = await Messages.find({
+      $or: [
+        {senderId: req.params.sender, receiverId: req.params.receiver},
+        {senderId: req.params.receiver, receiverId: req.params.sender}
+      ]
+    });
+    res.status(200).json(conversations);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+app.get('/userprofile/:userId', async (req, res) =>{
+  try {
+    const user = await Signup.findById(req.params.userId);
+    res.status(200).json(user);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+})
+
+
+
+app.post('/messages/new', (req, res)=>{
+    const dbMessage = req.body;
+
+    Messages.create(dbMessage, (err, data)=>{
+        if(err){
+            res.status(500).send(err);
+        }else{
+            res.status(201).send(data);
+        }
+    });
+});
 
  // Register user
- app.post('/signup/new', upload.single("profileImg"), async (req, res)=>{
+app.post('/signup/new', upload.single("profileImg"), async (req, res)=>{
     try {
         const fname = req.body.fname;
         const lname = req.body.lname;
@@ -110,13 +169,11 @@ app.get("/", (req, res) => res.status(200).send("Hello its working"));
     
         const salt = await bcrypt.genSalt();
         const passwordhash = await bcrypt.hash(password, salt);
-        const unique_id = crypto.randomBytes(16).toString("hex");
 
     
         // save a new user account to the db
     
         const newUser = new Signup({
-          unique_id,
           fname,
           lname,
           email,
@@ -134,8 +191,6 @@ app.get("/", (req, res) => res.status(200).send("Hello its working"));
           },
           process.env.JWT_SECRET
         );
-        console.log(token);
-        
     
         // send the token in a HTTP-only cookie
     
@@ -154,7 +209,7 @@ app.get("/", (req, res) => res.status(200).send("Hello its working"));
 
   app.get("/users/:uid", async (req, res) =>{
     try {
-      const user = await Signup.find({ unique_id: { $ne: req.params.uid}});
+      const user = await Signup.find({ _id: { $ne: req.params.uid}});
       res.status(200).json(user);
     } catch (err) {
       res.status(500).json(err);
@@ -162,58 +217,59 @@ app.get("/", (req, res) => res.status(200).send("Hello its working"));
       
   });
 
-  app.post("/login", async (req, res) => {
-    try {
-      const { email, password } = req.body;
-      
-      // validate
-      
-      if (!email || !password)
-        return res
-          .status(400)
-          .json({ errorMessage: "Please enter all required fields." });
-      
-      const existingUser = await Signup.findOne({ email });
-      if (!existingUser)
-        return res.status(401).json({ errorMessage: "Wrong email or password." });
-      
-      const passwordCorrect =  await bcrypt.compare(password, existingUser.passwordhash)
-      if (!passwordCorrect)
-        return res.status(401).json({ errorMessage: "Wrong email or password." });
-      
-      // sign the token
-      
-      const token = jwt.sign(
-        {
-          user: existingUser._id,
-        },
-        process.env.JWT_SECRET
-      );
-      
-      // send the token in a HTTP-only cookie
-      
-      
-      
-     res
-        .cookie("token", token, {
-          httpOnly: true,
-          secure: true,
-          sameSite: "none",
-        })
-        .send();
-    } catch (err) {
-      console.error(err);
-      res.status(500).send();
-    }
-  });
+
+    //validate to login the user
+    app.post("/login", async (req, res) => {
+      try {
+        const { email, password } = req.body;
+    
+        // validate
+    
+        if (!email || !password)
+          return res
+            .status(400)
+            .json({ errorMessage: "Please enter all required fields." });
+    
+        const existingUser = await Signup.findOne({ email });
+        if (!existingUser)
+          return res.status(401).json({ errorMessage: "Wrong email or password." });
+    
+        const passwordCorrect =  await bcrypt.compare(password, existingUser.passwordhash)
+        if (!passwordCorrect)
+          return res.status(401).json({ errorMessage: "Wrong email or password." });
+    
+        // sign the token
+    
+        const token = jwt.sign(
+          {
+            user: existingUser._id,
+          },
+          process.env.JWT_SECRET
+        );
+    
+        // send the token in a HTTP-only cookie
+    
+       res
+          .cookie("token", token, {
+            httpOnly: true,
+            secure: true,
+            sameSite: "none",
+          })
+          .send();
+      } catch (err) {
+        console.error(err);
+        res.status(500).send();
+      }
+    });
+     
     // logout the user
    app.get("/logout", (req, res) => {
       res
         .cookie("token", "", {
           httpOnly: true,
-          expires: new Date(0),
           secure: true,
           sameSite: "none",
+          expires: new Date(0),
         })
         .send();
     });
@@ -234,7 +290,6 @@ app.get("/", (req, res) => res.status(200).send("Hello its working"));
       app.get("/getToken", (req, res) => {
         try {
           const token = req.cookies.token;
-          console.log(token);
           if (!token) return res.json(false);
       
           jwt.verify(token, process.env.JWT_SECRET);
@@ -245,68 +300,7 @@ app.get("/", (req, res) => res.status(200).send("Hello its working"));
         }
       });
 
-      app.post('/messages/new', (req, res)=>{
-        const dbMessage = req.body;
-    
-        Messages.create(dbMessage, (err, data)=>{
-            if(err){
-                res.status(500).send(err);
-            }else{
-                res.status(201).send(data);
-            }
-        });
-    });
-
-      app.get('/messages/:sender/:receiver', async (req, res) => {
-        try {
-          const conversations = await Messages.find({
-            $or: [
-              {senderId: req.params.sender, receiverId: req.params.receiver},
-              {senderId: req.params.receiver, receiverId: req.params.sender}
-            ]
-          });
-          res.status(200).json(conversations);
-        } catch (err) {
-          res.status(500).json(err);
-        }
-      });
-
-      app.get("/chatUsers/:userId", async (req, res) =>{
-        try {
-          const id = req.params.userId;
-           const users = await Signup.aggregate([
-            {$match: { unique_id : id }},
-            {$lookup:{
-                from: 'messagecontents',
-                localField: 'unique_id',
-                foreignField: { $or: ['senderId', 'receiverId'] },
-                as : 'users'
-            }}
-            ])
-             .exec();
-             res.status(200).send(users)
-        } catch (err) {
-          res.status(500).json(err);
-        }
-      });
-
-      app.get('/chatUsers/:chatUserId', async (req, res) => {
-
-        try {
-          const chatmessages = await Messages.find({
-            $or :[
-               {senderId: req.params.chatUserId},
-               {receiverId: req.params.chatUserId},
-            ]
-          });
-          res.status(200).json(chatmessages);
-        } catch (err) {
-          res.status(500).json(err);
-        }
-      });
 
 
-
-
-// Listener
-app.listen(port, () => console.log(`listening on localhost: ${port}`));
+// listener
+app.listen(port, () => console.log(`Listening on lacalhost:${port}`));
